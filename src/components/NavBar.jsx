78e696e54b45bc806939fd88eaf52956c4d237cb
Ivan Ma <ivanma215@gmail.com>
2025-04-28 23:43:20 +1000

    responsive navbar to scroll

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,39 @@
-import { useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
-export const NavBar = ({ menuOpen, setMenuOpen, setHomeOpen, }) => {
+export const NavBar = ({ menuOpen, setMenuOpen, setHomeOpen }) => {
+    const [showNavBar, setShowNavBar] = useState(true);
+    const prevScrollY = useRef(0);
 
     useEffect(() => {
-        document.body.style.overflow = menuOpen ? "hidden" : ""; {/*Locks scrolling on page*/}
-    }, [menuOpen])
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    }, [menuOpen]);
+
+    // useEffect w/ [] used here to ENSURE only 1 event listener and cleanup
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            if (currentScrollY > prevScrollY.current && currentScrollY > 100) {
+                // Scrolling down
+                setShowNavBar(false);
+            } else {
+                // Scrolling up
+                setShowNavBar(true);
+            }
+            prevScrollY.current = currentScrollY;
+        };
+
+        window.addEventListener('scroll', handleScroll); // ongoing event listener w/ handleScroll attached!
+        return () => window.removeEventListener('scroll', handleScroll); // is saved, only occurs when component is destroyed 
+    }, []);
 
     return (
         <>
-            <div className="
+            <div className={`
+            ${showNavBar ? "translate-y-0" : "-translate-y-full"}
+            fixed z-50 duration-300 transition-transform
             flex justify-around items-center
             w-screen h-20
-            bg-gray-500 text-black" >
+            bg-gray-500 text-black`} >
                 <a> Ivan Ma </a>
 
                 <div className="hidden md:flex items-center space-x-8 "> 
